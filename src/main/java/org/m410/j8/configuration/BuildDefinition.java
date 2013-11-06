488afcff1ee1413069f809e2a405faca9a50ca52
Michael Fortin <michael@m410.us>
2013-11-05 22:31:25 -0500

    remodeled the server compiler

--- src/main/java/org/m410/j8/configuration/BuildDefinition.java
+++ src/main/java/org/m410/j8/configuration/BuildDefinition.java
@@ -3,7 +3,31 @@ package org.m410.j8.configuration;
 import java.util.Map;
 
 /**
- * Document Me..
+ *
+ *   default_task: "package"
+ compiler_type: "scala"
+ compiler_version: "2.10.0"
+ compiler_args: ""
+ target_dir: "{working_dir}/target"
+ webapp_dir: "{working_dir}/webapp"
+ package_classifier: ""
+ package_source: false
+ package_doc: false
+ deploy_target: ""
+ publish_target: ""
+ version_control: ""
+ fab_cache_dir: "{user.home}/.fab"
+ fab_home_dir: "{env.FAB_HOME}"
+ builder_dir: "{working_dir}/.fab"
+ builder_cache_dir: "{builder_dir}/fab"
+ builder_code_dir: "{builder_dir}/fab_code"
+ builder_lib_dir: "{builder_dir}/fab_lib"
+ source_dir: "{woking_dir}/src/scala"
+ resource_dir: "{woking_dir}/src/resources"
+ classes_dir: "{target_dir}/classes"
+ test_source_dir: "{working_dir}/test/scala"
+ test_resource_dir: "{working_dir}/test/resources"
+ test_classes_dir: "{target_dir}/test-classes"
  *
  * @author Michael Fortin
  */
