40b47ed64c976b434a1dd162d303228068df2795
Michael Fortin <michael@m410.us>
2014-10-18 22:05:55 -0400

    module configuration

--- src/main/java/org/m410/garden/orm/Activator.java
+++ src/main/java/org/m410/garden/orm/Activator.java
@@ -7,7 +7,6 @@ import org.osgi.framework.*;
  * @author Michael Fortin
  */
 public class Activator implements BundleActivator {
-    ServiceRegistration fabricateServiceRegistration;
 
     public void start(BundleContext context) throws Exception {
         ServiceReference fabricateServiceReference = context.getServiceReference(FabricateService.class.getName());
@@ -25,7 +24,7 @@ public class Activator implements BundleActivator {
     }
 
     public void stop(BundleContext context) throws Exception {
-        fabricateServiceRegistration.unregister();
+
     }
 
 }
